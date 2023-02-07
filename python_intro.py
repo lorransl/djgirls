bf4214a93e6aa0881d5aafb573b50510b20fec42
Lorran <lorran.sl@hotmail.com>
2023-02-07 00:40:42 -0300

    jubileu

---
+++ python_intro.py
@@ -0,0 +1,29 @@
+name = 'Lorran'
+if 3>2:
+  print(name + ', está na hora de deixar de ser um fodido.')
+if 5 > 2:
+  print('5 is indeed greater than 2')
+else:
+  print('5 is not greater than 2')
+  
+# name = 'Sonja'
+if name == 'Ola':
+  print('Hey Ola!')
+elif name == 'Sonja':
+  print('Hey Sonja!')
+else:
+  print('Hey anonymous!')
+  
+volume = 115
+if volume < 20:
+  print("It's kinda quiet.")
+elif 20 <= volume < 40:
+  print("It's nice for background music")
+elif 40 <= volume < 60:
+  print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+  print("Nice for parties")
+elif 80 <= volume < 100:
+  print("A bit loud!")
+else:
+  print("My ears are hurting! :(")
